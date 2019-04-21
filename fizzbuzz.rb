0eeccc37a70f06c2eec0b60a677aa58057345703
山崎祥司 <jacky@yamazakishouji-no-iMac.local>
2019-04-21 12:24:34 +0900

    implement ruby

---
+++ fizzbuzz.rb
@@ -0,0 +1,21 @@
+require 'time'
+
+start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+range = 1..1000000
+range.each{|num|
+    if ((num % 3 == 0) && (num % 5 == 0)) then
+        puts 'num= %d fizzbuzz' % [num]
+    elsif (num % 3 == 0) then
+        puts 'num= %d fizz' % [num]
+    elsif (num % 5 == 0) then
+        puts 'num= %d buzz' % [num]
+    else
+        puts 'num= %d' % [num]
+    end
+}
+
+finish = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+elapsed = finish - start
+puts elapsed
