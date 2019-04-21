510e378bf2784cda2ad00bfec63659d2017c0ff1
山崎祥司 <jacky@yamazakishouji-no-iMac.local>
2019-04-21 12:57:38 +0900

    update

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -5,13 +5,13 @@ start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 range = 1..1000000
 range.each{|num|
     if ((num % 3 == 0) && (num % 5 == 0)) then
-        puts 'num= %d fizzbuzz' % [num]
+        puts 'fizzbuzz'
     elsif (num % 3 == 0) then
-        puts 'num= %d fizz' % [num]
+        puts 'fizz'
     elsif (num % 5 == 0) then
-        puts 'num= %d buzz' % [num]
+        puts 'buzz'
     else
-        puts 'num= %d' % [num]
+        puts '%d' % [num]
     end
 }
 
